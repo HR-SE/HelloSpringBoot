18d1f69636e364842a398560c624e0b0c58e0275
Himanshu Ranjan <him.jbd@gmail.com>
2025-06-15 22:52:24 +0530

    Fixed Postman issues, tested POST /user and GET /users

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,15 +2,17 @@ package com.example.demo;
 
 //import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
 
 @SpringBootApplication
 @RestController
 public class HelloController {
+        private ArrayList<User> users = new ArrayList<>();
 
-          @GetMapping("/hello")
+        @GetMapping("/hello")
         public String sayHello() {
             return "Hello, Himanshu! How r u today?";
         }
@@ -28,5 +30,15 @@ public class HelloController {
         @GetMapping("/user/{name}")
         public String greetUser(@PathVariable String name) {
             return "Hello, " + name + "!";
+        }
+
+        @PostMapping("/user")
+        public String createUser(@RequestBody User user) {
+            users.add(user);
+            return "User created: " + user.getName() + ", " + user.getEmail();
+        }
+        @GetMapping("/users")
+        public ArrayList<User> getUsers() {
+            return users;
         }
     }

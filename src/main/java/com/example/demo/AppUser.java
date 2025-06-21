d57018e3311e215a3d68567572d17766688f768e
Himanshu Ranjan <him.jbd@gmail.com>
2025-06-21 19:43:54 +0530

    Completed H2 integration with age field

--- src/main/java/com/example/demo/AppUser.java
+++ src/main/java/com/example/demo/AppUser.java
@@ -9,13 +9,17 @@ public class AppUser {
     @Id
     private String name;
     private String email;
+    private int age;
     public AppUser() {}
-    public AppUser(String name, String email) {
+    public AppUser(String name, String email, int age) {
         this.name = name;
         this.email = email;
+        this.age = age;
     }
     public String getName() { return name; }
-    public String getEmail() { return email;}
+    public String getEmail() { return email; }
+    public int getAge() { return age; }
     public void setEmail(String email) { this.email = email; }
     public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
 }
--- src/main/java/com/example/demo/UserService.java
+++ src/main/java/com/example/demo/UserService.java
@@ -13,7 +13,7 @@ public class UserService {
             throw new IllegalArgumentException("User or name cannot be null");
         }
         userRepository.save(user);
-        return "User created: " + user.getName() + ", " + user.getEmail();
+        return "User created: " + user.getName() + ", " + user.getEmail() + ", Age : " + user.getAge();
     }
     public List<AppUser> getUsers() {
         return userRepository.findAll();
